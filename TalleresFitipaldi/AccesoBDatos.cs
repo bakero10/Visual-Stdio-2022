e62a70f114b4328cd13eedb0648f5fcc713fe9cb
Bakero10 <210162@iessierradeguara.com>
2023-02-11 19:15:49 +0100

    metodos de insertar,actualizar y borrar introducidos

--- TalleresFitipaldi/AccesoBDatos.cs
+++ TalleresFitipaldi/AccesoBDatos.cs
@@ -1,6 +1,13 @@
 ﻿using Devart.Data.MySql;
 using System;
+using System.Data;
 using System.Windows;
+using System.Windows.Controls;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.Windows.Documents;
+
 
 public class AccesoBDatos
 {
@@ -36,4 +43,28 @@ public class AccesoBDatos
         command.ExecuteNonQuery();
         myConnection.Close();
     }
+
+    public void actualizarCliente(int id, String nombre, String apellidos, String dni, int telefono, String direccion, String correo)
+    {
+       
+        String sql = "UPDATE cliente SET Nombre = '"+nombre+"', Apellidos = '"+apellidos+"', DNI = '"+dni+"',Telefono="+telefono+", Direccion='"+direccion+"',Correo='"+correo+"' WHERE idCliente = "+id;
+        MySqlCommand command = myConnection.CreateCommand();
+        command.CommandText = sql;
+        MessageBox.Show(sql);
+        command.ExecuteNonQuery();
+        myConnection.Close();
+    }
+    public DataTable rellenarDataGrid()
+    {
+        String sql = "SELECT * FROM cliente";
+        using (MySqlDataAdapter adapter = new MySqlDataAdapter(sql, myConnection))
+        {
+            // Crea un objeto DataTable y llena los datos utilizando el objeto SqlDataAdapter
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+
+        }
+    }
+
 }
--- TalleresFitipaldi/Cliente.xaml.cs
+++ TalleresFitipaldi/Cliente.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.Common;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -22,13 +23,14 @@ namespace TalleresFitipaldi
 
     public partial class Cliente : Window
     {
-        private DataGrid dg;
+        private DataGrid dataGrid;
         private AccesoBDatos abd;
         public Cliente()
         {
             InitializeComponent();
             abd= new AccesoBDatos();
-            dg = new DataGrid();    
+            dataGrid = new DataGrid();    
+
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -61,7 +63,15 @@ namespace TalleresFitipaldi
 
         private void botonActualizar_Click(object sender, RoutedEventArgs e)
         {
+            abd.actualizarCliente(int.Parse(textBoxId.Text),
+                                textBoxNombre.Text,
+                                textBoxApellido.Text,
+                                textBoxDNI.Text,
+                                int.Parse(textBoxTelefono.Text),
+                                textBoxDireccion.Text,
+                                textBoxCorreo.Text);
 
+            MessageBox.Show("Cliente Actualizado correctamente!", "Información");
         }
 
         private void botonBorrar_Click(object sender, RoutedEventArgs e)
@@ -74,6 +84,26 @@ namespace TalleresFitipaldi
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             
+        }
+        public void CargarDatos()
+        {
+            // Crea una instancia de la clase de conexión y llama al método para obtener los datos
+            AccesoBDatos conexion = new AccesoBDatos();
+            DataTable data = conexion.rellenarDataGrid();
+            if (data.Rows.Count > 0)
+            {
+                MessageBox.Show(data.ToString());
+            }
+            else
+            {
+                // El objeto DataTable está vacío, no se puede asignar como origen de datos
+                MessageBox.Show("El objeto DataTable está vacío, no se puede asignar como origen de datos");
+            }
+        }
+
+        private void botonBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            CargarDatos();
         }
     }
 }
